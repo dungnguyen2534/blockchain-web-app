1ee77c0407b33e1e5dbeb05a055a9f1a9b988682
dungnguyen2534 <ndunv2503@gmail.com>
2025-06-04 06:28:14 +0700

    Improve responsiveness

--- client/src/components/ui/ThemeList.tsx
+++ client/src/components/ui/ThemeList.tsx
@@ -42,7 +42,7 @@ export default function ThemeList() {
       <div className="modal-box duration-75 bg-base-200">
         <div className="font-bold text-lg mb-1">Choose a theme:</div>
 
-        <div className="flex flex-wrap gap-3">
+        <div className="flex flex-wrap gap-5">
           {themes.map((theme) => (
             <ThemeButton theme={theme} key={theme} />
           ))}
@@ -59,15 +59,16 @@ export default function ThemeList() {
 function ThemeButton({ theme }: { theme: string }) {
   return (
     <button
-      className="theme-controller inline-flex cursor-pointer"
+      className="theme-controller inline-flex cursor-pointer flex-grow"
       data-set-theme={theme}
       value={theme}
+      flex-grow
       data-theme={theme}
     >
-      <div className="bg-primary aspect-square h-5 border-2"></div>
-      <div className="bg-secondary aspect-square h-5 border-2"></div>
-      <div className="bg-accent aspect-square h-5 border-2"></div>
-      <div className="bg-neutral aspect-square h-5 border-2"></div>
+      <div className="bg-primary aspect-square h-6  border-2 flex-grow"></div>
+      <div className="bg-secondary aspect-square h-6  border-2 flex-grow"></div>
+      <div className="bg-accent aspect-square h-6  border-2 flex-grow"></div>
+      <div className="bg-neutral aspect-square h-6  border-2 flex-grow"></div>
     </button>
   );
 }
